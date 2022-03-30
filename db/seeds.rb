350c06ac9c6f89ca8b2804523b9fb9c7bb77cb91
Toanlee <leminhtoan1@dtu.edu.vn>
2022-03-30 09:30:26 +0700

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 User.create!(name: "Minh Toàn",
 email: "toan@gmail.com",
 password: "toan123",
-password_confirmation: "toan123", admin: true)
+password_confirmation: "toan123", 
+admin: true,
+activated: true,
+activated_at: Time.zone.now)
 
 99.times do |n|
     name = Faker::Name.name
@@ -17,5 +13,7 @@ password_confirmation: "toan123", admin: true)
     User.create!(name: name,
     email: email,
     password: password,
-    password_confirmation: password)
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now)
 end
