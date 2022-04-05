a9759f4ca63e70cd59ac77a37039e0eeb9ef5c6c
Toanlee <leminhtoan1@dtu.edu.vn>
2022-04-05 17:25:26 +0700

    Comment update v4 reply in reply

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,9 @@
 module CommentsHelper
+    def reply_name(reply)
+        # if current_user.id === reply.parent.user.id
+        #     return "Bạn"
+        # else
+            return reply.parent.user.name
+        # end
+    end
 end
